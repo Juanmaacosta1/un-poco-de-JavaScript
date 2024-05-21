12324de149fa5f9a9c203776dc1177f999e3adef
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-20 22:24:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//creo una faunción basica que suma 2 numeros 
+//le paso los parametros 
+function suma(num1,num2){
+    const  resultado= num1+num2;
+
+    //devuelvo lo que haga la funcion 
+    return(resultado);
+}
+
+//pongo lo que quiero sumar
+let resultado = suma(3, 5);
+//muestro por pantalla el resultado 
+console.log(resultado);
