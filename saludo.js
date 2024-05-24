83d12c7e28ebdd5bf5e09f1ed24ab59260438107
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-23 21:05:31 -0300

    Add files via upload

---
+++ saludo.js
@@ -0,0 +1,9 @@
+//creo una función 
+function saludar(nombre){
+    //devuelvo adios con el nombre
+    return "adios"+" "+ nombre ;
+}
+//llamo a la funcion para almacenar el resultado
+let saludo = saludar("juan");
+//imprimo el resultado
+console.log(saludo);
