e3dd952811197059f6e18262dd17662208acfbe3
Juanmaacosta1 <acostajuanma854@gmail.com>
2024-05-24 03:19:53 -0300

    Add files via upload

---
+++ PARoIMPAR.js
@@ -0,0 +1,9 @@
+function numpar (num1){
+    if (num1 %2==0) {
+        console.log("tu número es par ");
+    }else{ 
+        console.log("tu número es impar");
+    }
+}
+let numero=7;
+numpar(numero);
